e6905a44db5058cfe44d8d0d2d8d2f14dee9f91c
VituJ <jvboliveira0405@gmail.com>
2022-11-10 09:21:13 -0300

    main

--- integracao_html5_php/valor.php
+++ integracao_html5_php/valor.php
@@ -1,5 +1,7 @@
 <?php
     $valor = $_GET["v"]; 
     $rq    = sqrt($valor);
-    echo "A raiz quadrada de $valor = $rq";
+    echo "A raiz quadrada de $valor = " . number_format($rq,2) ;
 ?>
+<br>
+<a href="index.html">Voltar</a>
