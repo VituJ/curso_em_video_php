844e6138cb50b0aebcfbc9df9337a8181fc31c84
VituJ <jvboliveira0405@gmail.com>
2022-11-10 10:19:38 -0300

    main

--- integracao_html5_php/ex002/valor.php
+++ integracao_html5_php/ex002/valor.php
@@ -1,7 +1,9 @@
 <?php
-    $nome           = $_GET["nome"];
-    $ano_nascimento = $_GET["ano_nascimento"];
-    $sexo           = $_GET["sexo"];
+    $nome           = isset($_GET["nome"])?$_GET["nome"]:"[não informado]";
+    $ano_nascimento = isset($_GET["ano_nassscimento"])?$_GET["ano_nascimento"]:0;
+    $sexo           = isset($_GET["sexo"])?$_GET["sexo"]:"[sexo não informado]";
     $idade          = date("Y") - $ano_nascimento;
     echo "$nome é $sexo e tem $idade anos";
 ?>
+<br>
+<a href="index.html">Voltar</a>
